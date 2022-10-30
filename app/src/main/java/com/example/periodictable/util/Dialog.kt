7326e0d313ab34a2d561c52c0f7589b5f11c4155
enes <enesselcu25@gmail.com>
2022-10-30 11:00:41 +0300

    version 2.0

--- app/src/main/java/com/example/periodictable/util/Dialog.kt
+++ app/src/main/java/com/example/periodictable/util/Dialog.kt
@@ -2,32 +2,31 @@ package com.example.periodictable.util
 
 import android.content.Context
 import android.view.LayoutInflater
-import com.example.periodictable.databinding.AlertDesignBinding
 import com.example.periodictable.databinding.SearchDialogBinding
-import com.example.periodictable.model.DialogElements
-import com.example.periodictable.model.Elements
+import com.example.periodictable.source.model.ElementsDetails
+import com.example.periodictable.source.model.Elements
 
 
 object Dialog {
 
-    fun searchDialog(context: Context, list: DialogElements){
+    fun searchDialog(context: Context, list: ElementsDetails) {
         val binding = SearchDialogBinding.inflate(LayoutInflater.from(context))
         val dialog = context.let { android.app.Dialog(it) }
         dialog.setContentView(binding.root)
         binding.searchDialog = list
+        dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
         dialog.show()
-        binding.imageBClose.setOnClickListener{dialog.dismiss()}
-
+        binding.imageBClose.setOnClickListener { dialog.dismiss() }
     }
 
-    fun homeDialog(context: Context,elementsList:Elements){
-        val binding = AlertDesignBinding.inflate(LayoutInflater.from(context))
-        val dialog = context.let { android.app.Dialog(it) }
-
-        dialog.setContentView(binding.root)
-        binding.alert = elementsList
-        dialog.show()
-        binding.imaBtnClose.setOnClickListener { dialog.dismiss() }
-
-    }
+//    fun homeDialog(context: Context, elementsList: Elements) {
+//        val binding = ElementDialogBinding.inflate(LayoutInflater.from(context))
+//        val dialog = context.let { android.app.Dialog(it) }
+//
+//        dialog.setContentView(binding.root)
+//        binding.alert = elementsList
+//        dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+//        dialog.show()
+//        binding.imaBtnClose.setOnClickListener { dialog.dismiss() }
+//    }
 }
